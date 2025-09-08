6a4052ebf76f46cb061a63ce283c84855cdf9228
Prashant Ranjan <pr9899537195@gmail.com>
2025-09-08 23:30:21 +0530

    linked list ad first

--- dsalgo/LinkedLists.java
+++ dsalgo/LinkedLists.java
@@ -2,34 +2,43 @@ package dsalgo;
 
 public class LinkedLists {
 
-    public static class Node{
+    static class Node {
         int data;
         Node next;
-
-        Node (int data){
+        Node(int data) {
             this.data = data;
+            this.next = null;
         }
-    }
-    public static void main(String[] args) {
-        
-        System.out.println("working");
-        Node head = new Node(110);
-        Node middle = new Node(202);
-        Node last = new Node(301);
-
-
-        Node temp = head;
-
-        head.next = middle;
-        middle.next = last;
-        // last.next = null;
-
-        while (temp!= null) {
-            System.out.println(temp.data);
-            temp= temp.next;
-            
-        }
-
     }
     
+    Node head; // Declare head variable
+    
+    public void addFirst(int data){
+        Node newNode = new Node(data); // Create newNode inside method
+        //check if head is null
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head; // Link new node to current head
+        head = newNode; // Update head to new node
+    }
+    
+    public void print(){
+        Node current = head; // Use temporary variable to traverse
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println(); // Add newline after printing all elements
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("working");
+        LinkedLists list = new LinkedLists();
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(30);
+        list.print(); // Test the print method
+    }
 }
